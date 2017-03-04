79afd23c857f79f91c484a1945999b0ab52b695b
Hongyang Li <l3848378@outlook.com>
2017-03-04 15:58:22 -0800

    Create #7_ReverseInteger*.cpp

---
+++ 3_Math/#7_ReverseInteger*.cpp
@@ -0,0 +1,36 @@
+//#7
+//Easy
+//https://leetcode.com/problems/reverse-integer/?tab=Description
+//Arthor: lhy-loveworld
+//Method: Caution for overflow case!
+//        Another method is to use a long long type to do it
+//        The second method is a little slower
+//Complexity: time O(n); space O(1)
+
+class Solution {
+ public:
+  int reverse(int x) {
+    int sign = 1;
+    if (x < 0) sign = -1;
+    x = abs(x);
+    int ans = 0;
+    while (x != 0) {
+      if ((ans > INT_MAX / 10 )||(ans * 10 > INT_MAX - x % 10)) return 0;
+      else ans = ans * 10 + x % 10;
+      x = x / 10;
+    }
+    return sign * ans;
+  }
+};
+
+class Solution {
+ public:
+  int reverse(int x) {
+    long long ans = 0;
+    while (x) {
+      ans = ans * 10 + x % 10;
+      x /= 10;
+    }
+    return (ans < INT_MIN || ans > INT_MAX) ? 0 : ans;
+  }
+};

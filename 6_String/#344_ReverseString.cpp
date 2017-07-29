ecb522e1d4868ed52394cef4f5290caf8f74ef6b
lhy-loveworld <lhy-loveworld@github.com>
2017-07-28 21:58:01 -0700

    tree

---
+++ 6_String/#344_ReverseString.cpp
@@ -0,0 +1,17 @@
+//#344 Reverse String
+//Easy
+//https://leetcode.com/problems/reverse-string/tabs/description
+//Arthor: lhy-loveworld
+//Method: Use of string library
+//Complexity: Time O(N); Space O(1)
+class Solution {
+public:
+    string reverseString(string s) {
+        int len = s.length();
+        int i = 0, j = len - 1;
+        while (i < j) {
+            swap(s[i++], s[j--]);
+        }
+        return s;
+    }
+};
